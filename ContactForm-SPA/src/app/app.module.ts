e2a1d6e0629be33cf1cb2ae685b921dfbfc85c60
Mansoor Ahmed <mansooronly4u@gmail.com>
2019-01-22 00:18:56 +0530

    Added routing with home and contact component

--- ContactForm-SPA/src/app/app.module.ts
+++ ContactForm-SPA/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { appRoutes } from './routes';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      ContactComponent
+   ],
+   imports: [
+      BrowserModule,
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
