7cd834ae5c293a9649352fdaf20d923d520334d8
Mansoor Ahmed <mansooronly4u@gmail.com>
2019-01-22 01:50:04 +0530

    Added alertify references and contact page

--- ContactForm-SPA/src/app/contact/contact.component.ts
+++ ContactForm-SPA/src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { AlertifyService } from '../_service/alertify.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  contact: any = {};
+  baseurl  =  environment.apiUrl + 'enquiry/';
+  constructor(private http: HttpClient, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
+  addContact() {
+    this.http.post(this.baseurl, this.contact).subscribe(
+      response => {
+        this.alertify.success('Enquiry saved successfully');
+        this.contact = {};
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
+  }
 }
